1f61a22de54f25167e885d047619c32d2665a200
Mohamed WLN <114567817+MDDiallo9@users.noreply.github.com>
2023-12-06 20:09:46 +0100

    Added placeholders for inputs

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -12,21 +12,21 @@ const Form = () => {
       end: Date.now(),
     });
     setResult({years: dates.years,months : dates.months ,days : dates.days})
-    console.log(result)
+    console.log(result.years)
   }
   return (
     <>
     <form action="" className='flex flex-col items-center gap-6 ' onSubmit={(e) => handleInput(e)}>
         <div id="inputs" className='flex gap-3'>
-            <label className='uppercase flex flex-col' htmlFor="day">day<input required className='border-2 w-16 h-8 p-2 font-bold [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none' type="number" name='day'  /></label>
-            <label className='uppercase flex flex-col' htmlFor="month">month<input required className='border-2 w-16 h-8 p-2 font-bold [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none' type="number" name='month'  /></label>
-            <label className='uppercase flex flex-col' htmlFor="year">year<input required className='border-2 w-16 h-8 p-2 font-bold [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none' type="number" name='year'  /></label>
+            <label className='uppercase flex flex-col' htmlFor="day">day<input placeholder='DD' required className='border-2 w-16 h-8 p-2 font-bold [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none' type="number" name='day'  /></label>
+            <label className='uppercase flex flex-col' htmlFor="month">month<input placeholder='MM' required className='border-2 w-16 h-8 p-2 font-bold [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none' type="number" name='month'  /></label>
+            <label className='uppercase flex flex-col' htmlFor="year">year<input placeholder='YYYY' required className='border-2 w-16 h-8 p-2 font-bold [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none' type="number" name='year'  /></label>
         </div>
         <label className='' htmlFor="submit"><input type="submit" id='submit' value="" hidden/><div className='cursor-pointer bg-purple-500 w-16 h-16 rounded-full flex justify-center items-center'><img src="./assets/images/icon-arrow.svg" alt="" /></div></label>
         <div>
-        <p>{result.years} years</p>
-        <p>{result.months} months</p>
-        <p>{result.days} days</p>
+        <p><span>{result.years > 0 ? result.years : "- -"}</span> years</p>
+        <p><span>{result.months > 0 ? result.months : "- -"}</span> months</p>
+        <p><span>{result.days > 0 ? result.days : "- -"}</span> days</p>
         </div>
     </form>
     
