0cdc005e01b34066a3389b4c69a24dcb007fe6c6
Mohamed WLN <114567817+MDDiallo9@users.noreply.github.com>
2023-12-07 00:18:55 +0100

    CSS changes

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -16,17 +16,17 @@ const Form = () => {
   }
   return (
     <>
-    <form action="" className='flex flex-col items-center gap-6 ' onSubmit={(e) => handleInput(e)}>
-        <div id="inputs" className='flex gap-3'>
-            <label className='uppercase flex flex-col' htmlFor="day">day<input placeholder='DD' required className='border-2 w-16 h-8 p-2 font-bold [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none' type="number" name='day'  /></label>
-            <label className='uppercase flex flex-col' htmlFor="month">month<input placeholder='MM' required className='border-2 w-16 h-8 p-2 font-bold [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none' type="number" name='month'  /></label>
-            <label className='uppercase flex flex-col' htmlFor="year">year<input placeholder='YYYY' required className='border-2 w-16 h-8 p-2 font-bold [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none' type="number" name='year'  /></label>
+    <form action="" className='flex flex-col items-center gap-6 bg-white h-[400px] mx-2 w-full p-6 rounded-3xl rounded-br-[30%] lg:w-1/3 lg:w-[530px] ' onSubmit={(e) => handleInput(e)}>
+        <div id="inputs" className='flex gap-3 w-full lg:w-full'>
+            <label className='uppercase flex flex-col text-slate-400 text-sm' htmlFor="day">day<input placeholder='DD' required className='text-black border-2 w-full h-8 p-2 font-bold [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none lg:w-28' type="number" name='day'  /></label>
+            <label className='uppercase flex flex-col text-slate-400 text-sm' htmlFor="month">month<input placeholder='MM' required className='text-black border-2 w-full h-8 p-2 font-bold [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none lg:w-28' type="number" name='month'  /></label>
+            <label className='uppercase flex flex-col text-slate-400 text-sm' htmlFor="year">year<input placeholder='YYYY' required className='text-black border-2 w-full h-8 p-2 font-bold [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none lg:w-28' type="number" name='year'  /></label>
         </div>
-        <label className='' htmlFor="submit"><input type="submit" id='submit' value="" hidden/><div className='cursor-pointer bg-purple-500 w-16 h-16 rounded-full flex justify-center items-center'><img src="./assets/images/icon-arrow.svg" alt="" /></div></label>
-        <div>
-        <p><span>{result.years > 0 ? result.years : "- -"}</span> years</p>
-        <p><span>{result.months > 0 ? result.months : "- -"}</span> months</p>
-        <p><span>{result.days > 0 ? result.days : "- -"}</span> days</p>
+        <div className='relative w-full my-7'><label className='absolute -top-8 left-1/2 -translate-x-1/2 lg:left-[95%]' htmlFor="submit"><input type="submit" id='submit' value="" hidden/><div className='cursor-pointer bg-black hover:bg-purple-600 w-16 h-16 rounded-full flex justify-center items-center'><img src="./assets/images/icon-arrow.svg" alt="" /></div></label><hr></hr></div>
+        <div className='w-full lg:w-full '>
+        <p className='text-5xl font-black lg:text-6xl'><span className='text-purple-600'>{result.years > 0 ? result.years : "- -"}</span> years</p>
+        <p className='text-5xl font-black lg:text-6xl'><span className='text-purple-600'>{result.months > 0 ? result.months : "- -"}</span> months</p>
+        <p className='text-5xl font-black lg:text-6xl'><span className='text-purple-600'>{result.days > 0 ? result.days : "- -"}</span> days</p>
         </div>
     </form>
     
